9547d3272c113f34eaed24b152b2da8cd1803783
pythoro <pythoro@mindquip.com>
2020-05-01 16:10:35 +1200

    [IMP] Allow units to be multiplied and divided.

--- unitty/unit.py
+++ unitty/unit.py
@@ -15,6 +15,38 @@ def set_base(b):
     base = b
     unit_bases = b.bases
 
+
+def derive_mult(*unit_types):
+    types = list(unit_types)
+    types.sort()
+    if len(types) == 2:
+        t1, t2 = types
+        if t1 == 'area':
+            if t2 == 'length':
+                return 'volume'
+        elif t1 == 'length':
+            if t2 == 'length':
+                return 'area'
+            elif t2 == 'area':
+                return 'volume'
+        elif t1 == 'force':
+            if t2 == 'length':
+                return 'energy'
+    raise ValueError('Cannot multiply ' + str(types))
+
+
+def derive_div(*unit_types):
+    types = list(unit_types)
+    if len(types) == 2:
+        t1, t2 = types
+        if t1 == 'force':
+            if t2 == 'area':
+                return 'pressure'
+        elif t1 == 'mass':
+            if t2 == 'volume':
+                return 'density'
+    raise ValueError('Cannot divide ' + str(types))
+
 class Unit():
     def __init__(self, abbr, mult, unit_type, name):
         self.abbr = abbr
@@ -29,18 +61,38 @@ class Unit():
     def __repr__(self):
         return self.__str__()
 
-    def __rmatmul__(self, other):
-        return Quantity(other * self.mult, self.unit_type)
+    def __mul__(self, other):
+        if isinstance(other, Unit):
+            new_unit_type = derive_mult(self.unit_type, other.unit_type)
+            return Quantity(other.mult * self.mult, new_unit_type)
+        else:
+            return Quantity(other * self.mult, self.unit_type)
 
     def __rmul__(self, other):
+        return Quantity(other * self.mult, self.unit_type)
+
+    def __truediv__(self, other):
+        if isinstance(other, Unit):
+            new_unit_type = derive_div(self.unit_type, other.unit_type)
+            return Quantity(other.mult * self.mult, new_unit_type)
+        else:
+            return Quantity(other * self.mult, self.unit_type)
+
+    def __rtruediv__(self, other):
+        return Quantity(other * self.mult, self.unit_type)
+
+    def __rmatmul__(self, other):
         return other * self.mult
 
     def __rrshift__(self, other):
         return other / self.mult
 
+    
 
 class Units(dict):
     def __getattr__(self, abbr):
         if abbr in self:
             return self[abbr]
         return self.__getattribute__(abbr)
+    
+    
