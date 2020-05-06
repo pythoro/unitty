[FIX] Fix value of units in from_str method.

unitty/base.py
@@ -251,9 +251,9 @@ class Units():
             return self.units[s]
         spec = self.spec_from_str(s)
         index = self._ind(s)
-        value = 1.0
         units = [self.get_by_index(i) for i in spec]
         vector = np.sum([u.vector for u in units], axis=0)
+        value = np.prod([u.value for u in units])
         name = s
         utype = 0
         return self.new(index, value, vector, spec, name, utype)